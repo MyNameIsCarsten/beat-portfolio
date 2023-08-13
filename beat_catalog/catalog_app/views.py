27b04479e21e2439de9e4dfaee09f9ed6ca1a75d
Carsten Linder <Carsten@Carsten-Linder.de>
2023-08-13 15:16:20 +0200

    Styling changes to header

--- beat_catalog/catalog_app/views.py
+++ beat_catalog/catalog_app/views.py
@@ -7,9 +7,16 @@ def home(request):
   # Ausgewählte Objekte aus Datenbank holen
   all_items = Beat.objects.all()
   keys = Beat.objects.values('key').distinct()
-  min_bpm = Beat.objects.filter().order_by('bpm').first().bpm
-  max_bpm = Beat.objects.filter().order_by('bpm').last().bpm
+  try:
+    min_bpm = Beat.objects.filter().order_by('bpm').first().bpm
+  except:
+    min_bpm = 0
+  try:
+    max_bpm = Beat.objects.filter().order_by('bpm').last().bpm
+  except:
+    max_bpm = 200
   mid_bpm = round((min_bpm + max_bpm) / 2, 0)
+
   context = {
     'all_items': all_items,
     'keys': keys,
@@ -46,6 +53,14 @@ def home(request):
       # Ausgewählte Objekte aus Datenbank holen
       context['all_items'] = Beat.objects.filter(bpm=filter_value).values()
 
+    elif 'row_id' in request.POST:
+      id = request.POST['row_id']
+
+      # Objekt löschen
+      post_to_delete = Beat.objects.get(id=id)
+      post_to_delete.delete()
+
+      context['all_items'] = Beat.objects.all()
 
     else:
       folder_path = request.POST['path']
@@ -71,9 +86,16 @@ def home(request):
     # Alle Objekte aus Datenbank holen
     all_items = Beat.objects.all()
     keys = Beat.objects.values('key').distinct()
-
-    min_bpm = Beat.objects.filter().order_by('bpm').first().bpm
-    max_bpm = Beat.objects.filter().order_by('bpm').last().bpm
+    try:
+      min_bpm = Beat.objects.filter().order_by('bpm').first().bpm
+    except:
+      min_bpm = 0
+    try:
+      max_bpm = Beat.objects.filter().order_by('bpm').last().bpm
+    except:
+      max_bpm = 200
+    min_bpm = round(min_bpm, 0)
+    max_bpm = round(max_bpm, 0)
     mid_bpm = round((min_bpm + max_bpm)/2, 0)
 
     context = {
